441204d4a1d6ef63271fae5af3818bb8af4bb940
Theodore Kounas <teokounas@gmail.com>
2016-02-20 21:58:07 +0200

    upload initial navbar

---
+++ includes/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-default">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Project Name</a>
+    </div>
+    <div class="collapse navbar-collapse" id="navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href = '#about' >About</a></li>
+        <li><a href = '#search' >Search</a></li>
+        <li><a href = '#profile' >Profile</a></li>        
+      </ul>
+    </div>
+  </div><!-- /.container-fluid -->
+</nav>
