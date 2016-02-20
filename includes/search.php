943ef04621f8f60117daa52af0b041c7dbfc21a6
John Prantalos <johnprantalos@gmail.com>
2016-02-20 23:04:26 +0200

    Upload initial search page

---
+++ includes/search.php
@@ -0,0 +1,8 @@
+<div id="dropzone">
+	<form action="" method="post" class="dropzone needsclick dz-clickable" id="image-upload">
+		<div class="dz-message needsclick">
+    		Drop files here or click to upload.<br>
+    		<span class="note needsclick"></span>
+  </div>
+	</form>	
+</div>
