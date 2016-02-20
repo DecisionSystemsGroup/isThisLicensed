086e892f9cd1efd280e28e7ae1ce2b4b1574e26c
Theodore Kounas <teokounas@gmail.com>
2016-02-20 21:58:40 +0200

    upload initial index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<?php require_once("includes/head.php"); ?>
+	<body>
+		<?php require_once("includes/navbar.php") ?>
+		<div class="container">
+			<div class="row">			
+				<?php require_once("includes/search.php") ?>
+			</div>
+		</div>
+		<?php require_once("includes/footer.php") ?>
+		<?php require_once("includes/scripts.php") ?>
+	</body>
+</html>
