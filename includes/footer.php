8b865bc489d9f5ac0cee27ceaef552867d56ece3
John Prantalos <johnprantalos@gmail.com>
2016-02-20 22:43:53 +0200

    Add footer

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+	&copy; 2016. The <a href="http://dsg.teiste.gr/">Decision Systems Group</a> of <a href="http://teiste.gr/">University of Applied Sciences of Central Greece</a>.
+</footer>
