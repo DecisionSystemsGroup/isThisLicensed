55d117cde9119aef7905ad61d2396ebc8dd06876
Theodore Kounas <teokounas@gmail.com>
2016-02-20 21:57:55 +0200

    upload initial head

---
+++ includes/head.php
@@ -0,0 +1,12 @@
+<head>
+	<meta charset="utf-8" />
+	<title>Project Name | DSG on hack.summit() virtual hackathlon</title>
+
+	<!-- Vendor CSS -->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap-3.3.6-dist/css/bootstrap.min.css" />
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+</head>
