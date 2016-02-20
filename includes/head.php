f376f8e52c40902945489b00ee2d56319708e50b
John Prantalos <johnprantalos@gmail.com>
2016-02-20 22:46:36 +0200

    split css file to two temporary css files
    k for kounas and p for pandorian

--- includes/head.php
+++ includes/head.php
@@ -6,7 +6,9 @@
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap-3.3.6-dist/css/bootstrap.min.css" />
 
 	<!-- Custom CSS -->
-	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<!--<link rel="stylesheet" type="text/css" href="css/style.css" />-->
+	<link rel="stylesheet" type="text/css" href="css/k.style.css" />
+	<link rel="stylesheet" type="text/css" href="css/p.style.css" />
 	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 </head>
