f7d26df67973c1abd6602b583328a25ef0659f29
John Prantalos <johnprantalos@gmail.com>
2016-02-21 12:43:27 +0200

    Add navbar-brand img

--- includes/navbar.php
+++ includes/navbar.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.php">Project Name</a>
+      <a class="navbar-brand" href="index.php"><img src="img/brand.png" alt="brand-img" /></a>
     </div>
     <div class="collapse navbar-collapse" id="navbar-collapse">
       <ul class="nav navbar-nav navbar-right">
