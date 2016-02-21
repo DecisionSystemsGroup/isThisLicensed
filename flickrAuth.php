984cd99a49767778e1fc4a9c0c78b562b0d1b67d
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-02-21 20:49:12 +0200

    add flickrAuth, flickrImport functions
    flickAuth: If the user is'nt logged in he is redirected to flickr. If he
    is logged in he is redirected to flickrImport.
    flickImport: Checks if the user's token and id are stored to the
    session, then fetches all user images and imports them to our db.

---
+++ flickrAuth.php
@@ -0,0 +1,17 @@
+<?php
+	require './lib/utils.php';
+	
+    $f = new phpFlickr($api_key, $api_secret);
+	
+    if (empty($_GET['frob'])) {
+        $f->auth($permissions, false);
+    }
+	else {
+		session_start();
+		$f->auth_getToken($_GET['frob']);
+		$_SESSION['flickr_user_token'] = $f->parsed_response['auth']['token']['_content'];
+		$_SESSION['nsid'] = $f->parsed_response['auth']['user']['nsid'];
+		header("Location: ./flickrImport.php");
+		exit();
+	}
+?>
---
+++ flickrImport.php
@@ -0,0 +1,34 @@
+<?php
+	require './lib/utils.php';
+	if(!isset($_SESSION)){
+		session_start();
+	}
+	if(!isset($_SESSION['flickr_user_token'])||!isset($_SESSION['nsid'])){
+		header("Location: ./flickrAuth.php");
+		exit();
+	}
+    $f = new phpFlickr($api_key, $api_secret);
+	$f->setToken($_SESSION['flickr_user_token']);
+	$nsid = $_SESSION['nsid'];
+	   
+	$gall = $f->people_getPhotos($nsid,array('extras'=>'license,original_format'));
+
+	$num= count($gall['photos']['photo']);
+	$imgs=array();
+	for($i=0; $i<$num; $i++){
+	$url= 'https://farm'.$gall['photos']['photo'][$i]['farm'].'.staticflickr.com/'.$gall['photos']['photo'][$i]['server'].'/'.$gall['photos']['photo'][$i]['id'].'_'.$gall['photos']['photo'][$i]['originalsecret'].'_o.'.$gall['photos']['photo'][$i]['originalformat'];
+	
+
+		$imgs[]=array('system_id'=>$gall['photos']['photo'][$i]['id'], 
+					'system'=>'flickr',
+					'system_creator_id'=>$gall['photos']['photo'][$i]['owner'],
+					'url'=>$url,
+					'metadata'=>getImageMetadata($url)
+					);
+					
+	}
+	
+	$res = insertImages($imgs);
+	$response = array("success"=>$res?'true':'false', "total"=>count($imgs), "imported"=>$res?$res:0);
+	echo json_encode($response);
+?>
