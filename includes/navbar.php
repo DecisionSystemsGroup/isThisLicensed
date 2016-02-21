401111fa062c797594d342954a28f5033361fd0d
Theodore Kounas <teokounas@gmail.com>
2016-02-21 21:08:59 +0200

    button activation for loging in your yahho acc and flikr acc specificly

--- includes/navbar.php
+++ includes/navbar.php
@@ -16,7 +16,7 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Import <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#"><img src="img/fr.png">via Flickr</a></li>
+            <li><a href="flickrImport.php"><img src="img/fr.png">via Flickr</a></li>
           </ul>
         </li>       
       </ul>
