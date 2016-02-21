d5bd2432d0660061493325ca3a5b4191acc12c00
lilisath <lilis.athanasios@gmail.com>
2016-02-21 12:42:07 +0200

    init about page

--- about.php
+++ about.php
@@ -6,13 +6,8 @@
 		
 		<div class="container">
 			<div class="row">			
-				<div id="about">
-					<p>
-						<strong>
-							Creative Commons Corporation ("Creative Commons") is NOT a law firm and does not provide legal services or legal advice. Distribution of Creative Commons public licenses does not create a lawyer-client or other relationship. Creative Commons makes its licenses and related information available on an “as-is” basis. Creative Commons gives no warranties regarding its licenses, any material licensed under their terms and conditions, or any related information. Creative Commons disclaims all liability for damages resulting from their use to the fullest extent possible.
-						</strong>
-					</p>
-					
+				<div id="about" class="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
+					<br>					
 					Briefly...
 					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 					  <div class="panel panel-default"> 
@@ -137,6 +132,15 @@
 						</div>
 					  </div>
 					</div>
+					<br>
+					
+				</div>
+			</div>
+			<div class="row">						
+				<div class="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1" >
+					<p style="font-size:14px;">
+						Creative Commons Corporation ("Creative Commons") is NOT a law firm and does not provide legal services or legal advice. Distribution of Creative Commons public licenses does not create a lawyer-client or other relationship. Creative Commons makes its licenses and related information available on an “as-is” basis. Creative Commons gives no warranties regarding its licenses, any material licensed under their terms and conditions, or any related information. Creative Commons disclaims all liability for damages resulting from their use to the fullest extent possible.
+					</p>
 				</div>
 			</div>
 		</div>
