69a599718d55226ce1564d82afb22d98eb6f54ae
John Prantalos <johnprantalos@gmail.com>
2016-02-21 09:48:08 +0200

    Add Dropzone css and js

--- includes/head.php
+++ includes/head.php
@@ -4,6 +4,7 @@
 
 	<!-- Vendor CSS -->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap-3.3.6-dist/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="vendor/dropzone/css/dropzone.css" />
 
 	<!-- Custom CSS -->
 	<!--<link rel="stylesheet" type="text/css" href="css/style.css" />-->
