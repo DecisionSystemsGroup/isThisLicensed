f78a674dfc76df02d6251974c74218676f81fdf3
lilisath <lilis.athanasios@gmail.com>
2016-02-21 11:59:23 +0200

    init about page

---
+++ about.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+	<?php require_once("includes/head.php"); ?>
+	<body>
+		<?php require_once("includes/navbar.php") ?>
+		
+		<div class="container">
+			<div class="row">			
+				<div id="about">
+					<p>
+						<strong>
+							Creative Commons Corporation ("Creative Commons") is NOT a law firm and does not provide legal services or legal advice. Distribution of Creative Commons public licenses does not create a lawyer-client or other relationship. Creative Commons makes its licenses and related information available on an “as-is” basis. Creative Commons gives no warranties regarding its licenses, any material licensed under their terms and conditions, or any related information. Creative Commons disclaims all liability for damages resulting from their use to the fullest extent possible.
+						</strong>
+					</p>
+					
+					Briefly...
+					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+					  <div class="panel panel-default"> 
+						<div class="panel-heading" role="tab" id="headingOne">
+						  <h4 class="panel-title">
+							<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+							  <img  src="https://licensebuttons.net/l/by/3.0/88x31.png" alt=""></img> Attribution ( CC BY )
+							</a>
+						  </h4>
+						</div>
+						<div id="collapseOne" class="panel-collapse collapse " role="tabpanel" aria-labelledby="headingOne">
+						  <div class="panel-body">
+							Attribution means:
+							<p>
+								This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.
+							</p>
+							<h6>
+								<a href="//creativecommons.org/licenses/by/4.0">View License Deed</a>
+								|
+								<a href="//creativecommons.org/licenses/by/4.0/legalcode">View Legal Code</a>
+							</h6>
+						  </div>
+						</div>
+					  </div>
+					  <div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingTwo">
+						  <h4 class="panel-title">
+							<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+							  <img src="https://licensebuttons.net/l/by-nd/3.0/88x31.png" alt=""></img> Attribution-NoDerivs License ( CC BY-ND )
+							</a>
+						  </h4>
+						</div>
+						<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+						  <div class="panel-body">
+							No Derivative Works means:
+							<p>
+								This license allows for redistribution, commercial and non-commercial, as long as it is passed along unchanged and in whole, with credit to creator.
+							</p>
+							<a href="https://creativecommons.org/licenses/by-nd/4.0">View License Deed</a>
+							|
+							<a href="https://creativecommons.org/licenses/by-nd/4.0/legalcode">View Legal Code</a>
+						  </div>
+						</div>
+					  </div>
+					  <div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingThree">
+						  <h4 class="panel-title">
+							<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+							  <img src="https://licensebuttons.net/l/by-sa/3.0/88x31.png" alt=""></img> Attribution-ShareAlike ( CC BY-SA )
+							</a>
+						  </h4>
+						</div>
+						<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+						  <div class="panel-body">
+							Share Alike means:
+							<p>
+								This license lets others remix, tweak, and build upon creators work even for commercial purposes, as long as they credit creators and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.
+							</p>
+							<a href="https://creativecommons.org/licenses/by-sa/4.0">View License Deed</a>
+							|
+							<a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode">View Legal Code</a>
+						  </div>
+						</div>
+					  </div>
+					  <div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingFour">
+						  <h4 class="panel-title">
+							<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+							  <img src="https://licensebuttons.net/l/by-nc/3.0/88x31.png" alt=""></img> Attribution-NonCommercial ( CC BY-NC )
+							</a>
+						  </h4>
+						</div>
+						<div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+						  <div class="panel-body">
+							NonCommercial means:
+							<p>
+								This license lets others remix, tweak, and build upon creators work non-commercially, and although their new works must also acknowledge him and be non-commercial, they don’t have to license their derivative works on the same terms.
+							</p>
+							<a href="https://creativecommons.org/licenses/by-nc/4.0">View License Deed</a>
+							|
+							<a href="https://creativecommons.org/licenses/by-nc/4.0/legalcode">View Legal Code</a>
+						  </div>
+						</div>
+					  </div>
+					  <div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingFive">
+						  <h4 class="panel-title">
+							<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+							  <img src="https://licensebuttons.net/l/by-nc-sa/3.0/88x31.png" alt=""></img> Attribution-NonCommercial-ShareAlike ( CC BY-NC-SA)
+							</a>
+						  </h4>
+						</div>
+						<div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+						  <div class="panel-body">
+							NonCommercial-ShareAlike means:
+							<p>
+								This license lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms.
+							</p>
+							<a href="https://creativecommons.org/licenses/by-nc-sa/4.0">View License Deed</a>
+							|
+							<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode">View Legal Code</a>
+						  </div>
+						</div>
+					  </div>
+					  <div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingSix">
+						  <h4 class="panel-title">
+							<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+							  <img src="https://licensebuttons.net/l/by-nc-nd/3.0/88x31.png" alt=""></img> Attribution-NonCommercial-NoDerivs License ( CC BY-NC-ND )
+							</a>
+						  </h4>
+						</div>
+						<div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
+						  <div class="panel-body">
+							<p>
+								This license is the most restrictive of main licenses, only allowing others to download your works and share them with others as long as they credit the creator, but they can’t change them in any way or use them commercially.
+							</p>
+							<a href="https://creativecommons.org/licenses/by-nc-nd/4.0">View License Deed</a>
+							|
+							<a href="https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode">View Legal Code</a>
+						  </div>
+						</div>
+					  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php require_once("includes/footer.php") ?>
+		<?php require_once("includes/scripts.php") ?>
+	</body>
+</html>
