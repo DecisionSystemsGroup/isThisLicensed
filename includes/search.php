ee8f935e4158b7e63cedc467fb46939733ab55c0
John Prantalos <johnprantalos@gmail.com>
2016-02-21 11:38:42 +0200

    Change CSS for search.php

--- includes/search.php
+++ includes/search.php
@@ -1,28 +1,32 @@
 <div class="row">
-	<div class="col-md-8 col-md-offset-2">
+	<div class="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
 		<h1>How it works</h1>
 		<p>
 			Upload an image or give us an image url below, and we will check if this image is licensed somewhere in the Web!
 		</p>
-		<p>
-			Are you a content creator? See <a href="import.php">here</a> how you can import you images to isThisLicensed and declare licenses for them. This way you are making sure that when a user search an image of yours, the correct license appear.
-		</p>
+		<h3>Upload an image from your device</h3>	
 		<div id="dropzone">
-			<form action="index.php" method="post" class="dropzone needsclick dz-clickable" id="image-upload">
-				<div class="dz-message needsclick">
-		    		Drop files here or click to upload,<br>
-		    		<span class="note needsclick"></span>
-		  		</div>
-		  		<div class="form-group">
-			    	<label for="image-url">or give us an image url</label>
-			    	<div class="input-group ">
-				    	<div class="input-group-addon">http://</div>
-				    	<input type="text" class="form-control" id="image-url" placeholder="e.g. example.com/my-image" />
+			<form action="upload.php" method="post" class="dropzone" id="image-upload"></form>	
+		</div>
+		<h3>or enter an image URL below</h3>	
+		<div id="url-input">
+			<form class="form-inline" action="upload.php" method="post">
+				<div class="form-group">
+			    	<div class="input-group">
+				    	<input type="text" class="form-control" name="image-url" id="image-url" placeholder="e.g. http://example.com/my-image" size="40"/>
 			    		<span class="hidden" aria-hidden="true"></span>
 			    	</div>
 			  	</div>
-		  	</form>	
+				<div class="form-group">					
+			    	<input type="submit" name="submit" class="btn btn-default" value="Get File" />
+				</div>
+			</form>
 		</div>
+		<div id="info">
+			<p>
+				Are you a content creator? See <a href="import.php">here</a> how you can import you images to isThisLicensed and declare licenses for them. This way you are making sure that when a user search an image of yours, the correct license appear.
+			</p>
+		</div>		
 	</div>
 </div>
 
