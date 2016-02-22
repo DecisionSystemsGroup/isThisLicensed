bd0c801e44e97b5faece9ccb8c7c966431041cc1
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-02-22 08:59:10 +0200

    add licenses data

---
+++ lib/licenses.php
@@ -0,0 +1,14 @@
+<?php
+	$licenses = array(	0=>["type"=>"All Rights Reserved", "img"=>"http://169.45.235.85/img/copy.png"],
+					1=>["type"=>"Attribution-NonCommercial-ShareAlike License", "url"=>"http://creativecommons.org/licenses/by-nc-sa/2.0/", "img"=>"https://licensebuttons.net/l/by-nc-sa/3.0/88x31.png"],
+					2=>["type"=>"Attribution-NonCommercial License", "url"=>"http://creativecommons.org/licenses/by-nc/2.0/", "img"=>"https://licensebuttons.net/l/by-nc/3.0/88x31.png"],
+					3=>["type"=>"Attribution-NonCommercial-NoDerivs License", "url"=>"http://creativecommons.org/licenses/by-nc-nd/2.0/", "img"=>"https://licensebuttons.net/l/by-nc-nd/3.0/88x31.png"],
+					4=>["type"=>"Attribution License", "url"=>"http://creativecommons.org/licenses/by/2.0/", "img"=>"https://licensebuttons.net/l/by/3.0/88x31.png"],
+					5=>["type"=>"Attribution-ShareAlike License", "url"=>"http://creativecommons.org/licenses/by-sa/2.0/","img"=>"https://licensebuttons.net/l/by-sa/3.0/88x31.png"],
+					6=>["type"=>"Attribution-NoDerivs License", "url"=>"http://creativecommons.org/licenses/by-nd/2.0/", "img"=>"https://licensebuttons.net/l/by-nd/3.0/88x31.png"],
+					7=>["type"=>"No known copyright restrictions", "url"=>"http://flickr.com/commons/usage/", "img"=>"http://169.45.235.85/img/nc.png"],
+					8=>["type"=>"United States Government Work", "url"=>"http://www.usa.gov/copyright.shtml", "img"=>"http://169.45.235.85/img/usa.png"],
+					9=>["type"=>"Public Domain Work", "url"=>"https://creativecommons.org/publicdomain/mark/1.0/", "img"=>"http://i.creativecommons.org/p/mark/1.0/88x31.png"],
+					10=>["type"=>"Public Domain Dedication (CC0)", "url"=>"https://creativecommons.org/publicdomain/zero/1.0/", "img"=>"http://i.creativecommons.org/p/zero/1.0/88x31.png"]
+		);
+?>
