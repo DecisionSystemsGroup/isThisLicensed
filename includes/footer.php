ea2a4c268d0c1459d701de99e2da140beb31ac60
John Prantalos <johnprantalos@gmail.com>
2016-02-22 08:35:53 +0200

    Update footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,4 @@
 <footer>
-<img src="/img/dsg.png">
-<br>
-<a href="https://www.youtube.com/channel/UCb4w_1fp8ef-eka0wJ1--Ow"><i class="fa fa-youtube-square fa-3x	"></i></a>
-	<a href="https://plus.google.com/u/0/106701848379617958990/videos"><i class="fa fa-google-plus-square fa-3x	"></i></a>
-	<a href="https://www.facebook.com/dsg.teiste"><i class="fa fa-facebook-square fa-3x	"></i></a>
-	<a href="https://github.com/DecisionSystemsGroup"><i class="fa fa-github fa-3x"></i></a>
-	<br>
-	&copy; 2016. The <a href="http://dsg.teiste.gr/">Decision Systems Group</a> of <a href="http://teiste.gr/">University of Applied Sciences of Central Greece</a>.<br>
-	
+	&copy; 2016. The <a href="http://dsg.teiste.gr/">Decision Systems Group</a> of <a href="http://teiste.gr/">University of Applied Sciences of Central Greece</a>.	
+	<a href="http://dsg.teiste.gr/"><img src="/img/dsg.png"></a>
 </footer>
